86a304f41aeaedafae0ddce2850a2d68c140ed1a
oalt <oa@lacheweg21.de>
2024-07-18 17:25:48 +0200

    Initial code commit

---
+++ src/MDD4All.DevOpsObserver.StatusLightControl.Contracts/IStatusLightController.cs
@@ -0,0 +1,20 @@
+﻿using MDD4All.DevOpsObserver.DataModels;
+using System.Threading.Tasks;
+
+namespace MDD4All.DevOpsObserver.StatusLightControl.Contracts
+{
+    public interface IStatusLightController
+    {
+        bool IsOn { get; }
+
+        DevOpsStatus CurrentStatus { get; }
+
+        Task TurnLightOnAsync();
+
+        Task TurnLightOffAsync();
+
+        Task SetStatusLightAsync(DevOpsStatus devOpsStatus);
+
+        Task ActivateAlertAsync();
+    }
+}
